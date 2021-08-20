b306aee4836032f42dedf692cc0714124e6d76dc
DESKTOP-6L9RGIG\vantan <siso930212@gmail.com>
2021-08-20 13:20:07 +0900

    スクリプトに変更を加えた

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,12 +7,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("めめめ");
+        //Debug.Log("めめめ");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("めかぶ");
     }
 }
